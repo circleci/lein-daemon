536c4163fcfbeb0407e5922516951cb0ca56058c
J. David Lowe <j.david.lowe@gmail.com>
2014-05-31 11:58:27 -0700

    fix URL, bump again

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject circleci/lein-daemon "0.5.5"
+(defproject circleci/lein-daemon "0.5.6"
   :description "A lein plugin that daemonizes a clojure process"
-  :url "https://github.com/arohner/leiningen"
+  :url "https://github.com/circleci/lein-daemon"
   :license {:name "Eclipse Public License"}
   :eval-in-leiningen true
   :profiles {:dev
